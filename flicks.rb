c5e110016f2ec6379cd4fb6015fa6390ec307f02
cyberplanner <sal@salweb.co.uk>
2016-12-12 05:09:28 +0000

    Initial commit

---
+++ flicks.rb
@@ -0,0 +1,9 @@
+puts "Mikey loves Goonies"
+puts "Mikey's favourite movie is Goonies"
+
+movie = "Goonies"
+puts "Mikey's favourite movie is #{movie}"
+
+rank = 10
+
+puts "#{movie} \thas a rank of \n#{rank * 2}"
